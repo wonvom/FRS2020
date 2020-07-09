39a7f031a498696df71e345b8e961a53a721bd7e
Ethan9438 <knowjo94@gmail.com>
2020-07-09 17:43:21 +0900

    Contact-us 페이지 수정

--- src/views/examples/ContactUs.js
+++ src/views/examples/ContactUs.js
@@ -1,11 +1,11 @@
 import React from "react";
 // react plugin used to create google maps
-import {
-  withScriptjs,
-  withGoogleMap,
-  GoogleMap,
-  Marker
-} from "react-google-maps";
+// import {
+//   withScriptjs,
+//   withGoogleMap,
+//   GoogleMap,
+//   Marker
+// } from "react-google-maps";
 // reactstrap components
 import { Button, Form, Input, Container, Row, Col } from "reactstrap";
 
@@ -13,195 +13,195 @@ import { Button, Form, Input, Container, Row, Col } from "reactstrap";
 import InfoNavbar from "components/Navbars/ColorNavbar.js";
 import FooterWhite from "components/Footers/FooterBlack.js";
 
-const MapWrapper = withScriptjs(
-  withGoogleMap(props => (
-    <GoogleMap
-      defaultZoom={14}
-      defaultCenter={{ lat: 37.321128, lng: 127.128350 }}
-      defaultOptions={{
-        styles: [
-          {
-            featureType: "water",
-            elementType: "geometry",
-            stylers: [
-              {
-                color: "#e9e9e9"
-              },
-              {
-                lightness: 17
-              }
-            ]
-          },
-          {
-            featureType: "landscape",
-            elementType: "geometry",
-            stylers: [
-              {
-                color: "#f5f5f5"
-              },
-              {
-                lightness: 20
-              }
-            ]
-          },
-          {
-            featureType: "road.highway",
-            elementType: "geometry.fill",
-            stylers: [
-              {
-                color: "#ffffff"
-              },
-              {
-                lightness: 17
-              }
-            ]
-          },
-          {
-            featureType: "road.highway",
-            elementType: "geometry.stroke",
-            stylers: [
-              {
-                color: "#ffffff"
-              },
-              {
-                lightness: 29
-              },
-              {
-                weight: 0.2
-              }
-            ]
-          },
-          {
-            featureType: "road.arterial",
-            elementType: "geometry",
-            stylers: [
-              {
-                color: "#ffffff"
-              },
-              {
-                lightness: 18
-              }
-            ]
-          },
-          {
-            featureType: "road.local",
-            elementType: "geometry",
-            stylers: [
-              {
-                color: "#ffffff"
-              },
-              {
-                lightness: 16
-              }
-            ]
-          },
-          {
-            featureType: "poi",
-            elementType: "geometry",
-            stylers: [
-              {
-                color: "#f5f5f5"
-              },
-              {
-                lightness: 21
-              }
-            ]
-          },
-          {
-            featureType: "poi.park",
-            elementType: "geometry",
-            stylers: [
-              {
-                color: "#dedede"
-              },
-              {
-                lightness: 21
-              }
-            ]
-          },
-          {
-            elementType: "labels.text.stroke",
-            stylers: [
-              {
-                visibility: "on"
-              },
-              {
-                color: "#ffffff"
-              },
-              {
-                lightness: 16
-              }
-            ]
-          },
-          {
-            elementType: "labels.text.fill",
-            stylers: [
-              {
-                saturation: 36
-              },
-              {
-                color: "#333333"
-              },
-              {
-                lightness: 40
-              }
-            ]
-          },
-          {
-            elementType: "labels.icon",
-            stylers: [
-              {
-                visibility: "off"
-              }
-            ]
-          },
-          {
-            featureType: "transit",
-            elementType: "geometry",
-            stylers: [
-              {
-                color: "#f2f2f2"
-              },
-              {
-                lightness: 19
-              }
-            ]
-          },
-          {
-            featureType: "administrative",
-            elementType: "geometry.fill",
-            stylers: [
-              {
-                color: "#fefefe"
-              },
-              {
-                lightness: 20
-              }
-            ]
-          },
-          {
-            featureType: "administrative",
-            elementType: "geometry.stroke",
-            stylers: [
-              {
-                color: "#fefefe"
-              },
-              {
-                lightness: 17
-              },
-              {
-                weight: 1.2
-              }
-            ]
-          }
-        ],
-        scrollwheel: false //we disable de scroll over the map, it is a really annoing when you scroll through page
-      }}
-    >
-      <Marker position={{ lat: 37.321128, lng: 127.128350 }} />
-    </GoogleMap>
-  ))
-);
+// const MapWrapper = withScriptjs(
+//   withGoogleMap(props => (
+//     <GoogleMap
+//       defaultZoom={14}
+//       defaultCenter={{ lat: 37.321128, lng: 127.128350 }}
+//       defaultOptions={{
+//         styles: [
+//           {
+//             featureType: "water",
+//             elementType: "geometry",
+//             stylers: [
+//               {
+//                 color: "#e9e9e9"
+//               },
+//               {
+//                 lightness: 17
+//               }
+//             ]
+//           },
+//           {
+//             featureType: "landscape",
+//             elementType: "geometry",
+//             stylers: [
+//               {
+//                 color: "#f5f5f5"
+//               },
+//               {
+//                 lightness: 20
+//               }
+//             ]
+//           },
+//           {
+//             featureType: "road.highway",
+//             elementType: "geometry.fill",
+//             stylers: [
+//               {
+//                 color: "#ffffff"
+//               },
+//               {
+//                 lightness: 17
+//               }
+//             ]
+//           },
+//           {
+//             featureType: "road.highway",
+//             elementType: "geometry.stroke",
+//             stylers: [
+//               {
+//                 color: "#ffffff"
+//               },
+//               {
+//                 lightness: 29
+//               },
+//               {
+//                 weight: 0.2
+//               }
+//             ]
+//           },
+//           {
+//             featureType: "road.arterial",
+//             elementType: "geometry",
+//             stylers: [
+//               {
+//                 color: "#ffffff"
+//               },
+//               {
+//                 lightness: 18
+//               }
+//             ]
+//           },
+//           {
+//             featureType: "road.local",
+//             elementType: "geometry",
+//             stylers: [
+//               {
+//                 color: "#ffffff"
+//               },
+//               {
+//                 lightness: 16
+//               }
+//             ]
+//           },
+//           {
+//             featureType: "poi",
+//             elementType: "geometry",
+//             stylers: [
+//               {
+//                 color: "#f5f5f5"
+//               },
+//               {
+//                 lightness: 21
+//               }
+//             ]
+//           },
+//           {
+//             featureType: "poi.park",
+//             elementType: "geometry",
+//             stylers: [
+//               {
+//                 color: "#dedede"
+//               },
+//               {
+//                 lightness: 21
+//               }
+//             ]
+//           },
+//           {
+//             elementType: "labels.text.stroke",
+//             stylers: [
+//               {
+//                 visibility: "on"
+//               },
+//               {
+//                 color: "#ffffff"
+//               },
+//               {
+//                 lightness: 16
+//               }
+//             ]
+//           },
+//           {
+//             elementType: "labels.text.fill",
+//             stylers: [
+//               {
+//                 saturation: 36
+//               },
+//               {
+//                 color: "#333333"
+//               },
+//               {
+//                 lightness: 40
+//               }
+//             ]
+//           },
+//           {
+//             elementType: "labels.icon",
+//             stylers: [
+//               {
+//                 visibility: "off"
+//               }
+//             ]
+//           },
+//           {
+//             featureType: "transit",
+//             elementType: "geometry",
+//             stylers: [
+//               {
+//                 color: "#f2f2f2"
+//               },
+//               {
+//                 lightness: 19
+//               }
+//             ]
+//           },
+//           {
+//             featureType: "administrative",
+//             elementType: "geometry.fill",
+//             stylers: [
+//               {
+//                 color: "#fefefe"
+//               },
+//               {
+//                 lightness: 20
+//               }
+//             ]
+//           },
+//           {
+//             featureType: "administrative",
+//             elementType: "geometry.stroke",
+//             stylers: [
+//               {
+//                 color: "#fefefe"
+//               },
+//               {
+//                 lightness: 17
+//               },
+//               {
+//                 weight: 1.2
+//               }
+//             ]
+//           }
+//         ],
+//         scrollwheel: false //we disable de scroll over the map, it is a really annoing when you scroll through page
+//       }}
+//     >
+//       <Marker position={{ lat: 37.321128, lng: 127.128350 }} />
+//     </GoogleMap>
+//   ))
+// );
 
 function ContactUs() {
   document.documentElement.classList.remove("nav-open");
@@ -228,31 +228,7 @@ function ContactUs() {
                 </p>
               </Col>
             </Row>
-            {/* <Row>
-              <Col className="ml-auto mr-auto text-center" md="6">
-                <h3 className="title">
-                  <small>Find us on social networks</small>
-                </h3>
-                <Button className="btn-just-icon mr-1" color="twitter">
-                  <i className="fa fa-twitter" />
-                </Button>
-                <Button className="btn-just-icon mr-1" color="facebook">
-                  <i className="fa fa-facebook" />
-                </Button>
-                <Button className="btn-just-icon mr-1" color="google">
-                  <i className="fa fa-google" />
-                </Button>
-                <Button className="btn-just-icon mr-1" color="dribbble">
-                  <i className="fa fa-dribbble" />
-                </Button>
-                <Button className="btn-just-icon mr-1" color="instagram">
-                  <i className="fa fa-instagram" />
-                </Button>
-                <Button className="btn-just-icon" color="youtube">
-                  <i className="fa fa-youtube" />
-                </Button>
-              </Col>
-            </Row> */}
+            
             <Row>
               <Col className="ml-auto mr-auto text-center" md="6">
                 <h3 className="title">
@@ -285,21 +261,25 @@ function ContactUs() {
                   </Row>
                 </Form>
                 <h3 className="visit">
-                  <small>Or come and visit</small>
+                  <small>Or come and visit here</small>
+                </h3>
+                <h3 className="visit">
+                  <small>152, Jukjeon-ro, Suji-gu, Yongin-si, Gyeonggi-do, Republic of Korea</small>
                 </h3>
               </Col>
             </Row>
           </Container>
         </div>
       </div>
-      <div className="big-map" id="contactUsMap">
+      {/* <div className="big-map" id="contactUsMap">
         <MapWrapper
-          googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyADHgbbaA5Jea7ICklVUITn7TiHIQ5ckh8"
+          //googleMapURL="https://maps.googleapis.com/maps/api/js?libraries=places&key=AIzaSyBfosBbi1njLv8yqZbJkWX2w8yJxP4Yr3E"
+          googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBfosBbi1njLv8yqZbJkWX2w8yJxP4Yr3E"
           loadingElement={<div style={{ height: `100%` }} />}
           containerElement={<div style={{ height: `100%` }} />}
           mapElement={<div style={{ height: `100%` }} />}
         />
-      </div>
+      </div> */}
       <FooterWhite />
     </>
   );
