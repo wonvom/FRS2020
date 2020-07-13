1a101d44669f7b5a270b940ec083540e1fe120e0
Yeonji0660 <yeonji0660@gmail.com>
2020-07-13 20:13:47 +0900

    register page 개인 옵션 수정

--- src/views/examples/RegisterPage.js
+++ src/views/examples/RegisterPage.js
@@ -79,6 +79,7 @@ function RegisterPage() {
                             <Input defaultValue="" type="checkbox" />10s
                             <span className="form-check-sign" />
                           </Label>
+                          &nbsp;&nbsp;&nbsp;&nbsp;
                           <Label check>
                             <Input defaultValue="" type="checkbox" />20s
                             <span className="form-check-sign" />
@@ -89,6 +90,7 @@ function RegisterPage() {
                             <Input defaultValue="" type="checkbox" />30s
                             <span className="form-check-sign" />
                           </Label>
+                          &nbsp;&nbsp;&nbsp;
                           <Label check>
                             <Input defaultValue="" type="checkbox" />40s
                             <span className="form-check-sign" />
@@ -179,6 +181,7 @@ function RegisterPage() {
                             <Input defaultValue="" type="checkbox" />Female
                             <span className="form-check-sign" />
                           </Label>
+                          &nbsp;&nbsp;&nbsp;
                           <Label check>
                             <Input defaultValue="" type="checkbox" />Male
                             <span className="form-check-sign" />
